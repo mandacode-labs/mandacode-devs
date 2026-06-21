7317f90f25a12858fa821c098768a98f94c647a7
sauce <sauce@mandacode.com>
2026-06-21 13:04:57 +0900

    fix(editor): only render Mermaid UI for mermaid language code blocks (#40)

--- src/components/tiptap-node/mermaid-node/mermaid-node-extension.ts
+++ src/components/tiptap-node/mermaid-node/mermaid-node-extension.ts
@@ -1,21 +1,12 @@
 import { CodeBlockLowlight } from "@tiptap/extension-code-block-lowlight";
 import { ReactNodeViewRenderer } from "@tiptap/react";
-import { createLowlight, common } from "lowlight";
 import { MermaidNode as MermaidNodeComponent } from "@/components/tiptap-node/mermaid-node/mermaid-node";
-
-const lowlight = createLowlight(common);
 
 export const MermaidCodeBlock = CodeBlockLowlight.extend({
   name: "codeBlock",
 
   addNodeView() {
     return ReactNodeViewRenderer(MermaidNodeComponent);
-  },
-}).configure({
-  lowlight,
-  defaultLanguage: "plaintext",
-  HTMLAttributes: {
-    class: "language-",
   },
 });
 
